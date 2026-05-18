4d5c544ed00a1b2d4764974cf6b0fada00da069b
Vsevollord <Vsevollords@gmail.com>
2026-05-18 11:12:05 +0300

    add_power

--- main.py
+++ main.py
@@ -22,12 +22,18 @@ def sqrt(a):
 def square(a):
     return a**2
 
+def power(a, b):
+    if b < 0:
+        return 1 / (a ** abs(b))
+    return a ** b
+
 def main():
     print("Simple Calculator")
     print(f"2 + 3 = {add(2, 3)}")
     print(f"5 - 2 = {subtract(5, 2)}")
     print(f"sqrt(25) = {sqrt(25)}")
     print(f"4**2 = {square(4)}")
+    print(f"4 ** 3 = {power(4, 3)}")
 
 if __name__ == "__main__":
     main()
